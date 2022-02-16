4fb25befe954f2c297d34089cde9b45712f78eec
roznerx <ignaciorosner@gmail.com>
2022-02-16 01:12:37 -0300

    Still working

--- src/modules/itemPopUp.js
+++ src/modules/itemPopUp.js
@@ -37,6 +37,7 @@ function inputFieldMaker(id, placeholder) {
     return itemPopUpInput;
 }
 
+//TITLE, DESCRIPTION AND LOCATION
 let titleInputField = inputFieldMaker("title-input-field", "Enter Title");
 let descriptionInputFiled = inputFieldMaker("description-input-field", "Enter Description");
 let deadlineInputField = inputFieldMaker("deadline-input-field", "Enter Deadline");
@@ -45,14 +46,13 @@ let deadlineInputField = inputFieldMaker("deadline-input-field", "Enter Deadline
 //let locationInputField = inputFieldMaker("location-input-field", "Select Location");
 
 //PRIORITY
-
 let priorityInputField = document.createElement("div");
 priorityInputField.className = "item-input-field";
 priorityInputField.id = "priority-input-field";
 itemPopUpInputContainer.appendChild(priorityInputField);
 
-let selectedPriority = undefined;
 let prioritiesArr = [];
+let selectedPriority = undefined;
 
 function priorityMaker(id, innerHTML) {
     let priorityType = document.createElement("div");
@@ -61,57 +61,81 @@ function priorityMaker(id, innerHTML) {
     priorityType.innerHTML = innerHTML;
     priorityInputField.appendChild(priorityType);
     prioritiesArr.push(priorityType);
-    /*
-    priorityType.onclick = (e) => {
+    priorityType.addEventListener("click", () => {
         selectedPriority = priorityType;
-        priorityType.style.backgroundColor = "#222323";
-        priorityType.style.color = "#f0f6f0";
-        priorityType.style.padding = "1%, 0, 2%, 2%";
-        for (let i = 0; i <= prioritiesArr.length; i++) {
-            if (prioritiesArr[i] != selectedPriority) {
-                prioritiesArr[i].style.backgroundColor = "#f0f6f0";
-                prioritiesArr[i].style.color = "#222323";
-                prioritiesArr[i].style.padding = "0";
-            }
-        }
-    }
-    */
+        console.log(selectedPriority);
+    })
+    return priorityType;
 };
 
 let lowPriority = priorityMaker("low-priority", "Low Priority");
 let midPriority = priorityMaker("mid-priority", "Mid Priority");
 let highPriority = priorityMaker("high-priority", "High Priority");
 
-console.log(prioritiesArr[1]);
-
-
-
-/*
-for (let i = 0; i <= prioritiesArr.length; i++) {
-    lowPriority.onclick = () => {
-        //selectedPriority = prioritiesArr[i];
-        lowPriority.style.backgroundColor = "#222323";
-        lowPriority.style.color = "#f0f6f0";
-        lowPriority.style.padding = "1%, 0, 2%, 2%";
+prioritiesArr.forEach(p => p.addEventListener("click", () => {
+    if (selectedPriority = p) {
+        p.style.backgroundColor = "#222323";
+        p.style.color = "#f0f6f0";
+        p.style.padding = "1%, 0, 2%, 2%";
+    } else if (selectedPriority != p) {
+        p.style.backgroundColor = "#f0f6f0";
+        p.style.color = "#222323";
+        p.style.padding = "0";
     }
-}*/
+}));
 
 /*
-priorityType.onclick = (e) => {
-    selectedPriority = priorityType;
-    priorityType.style.backgroundColor = "#222323";
-    priorityType.style.color = "#f0f6f0";
-    priorityType.style.padding = "1%, 0, 2%, 2%";
-    for (let i = 0; i <= prioritiesArr.length; i++) {
-        if (prioritiesArr[i] != selectedPriority) {
+for (let i = 0; i < prioritiesArr.length; i++) {
+    prioritiesArr[i].addEventListener("click", () => {
+        if (selectedPriority = prioritiesArr[i]) {
+            selectedPriority.style.backgroundColor = "#222323";
+            selectedPriority.style.color = "#f0f6f0";
+            selectedPriority.style.padding = "1%, 0, 2%, 2%";
+        }
+        if (selectedPriority != prioritiesArr[i]) {
             prioritiesArr[i].style.backgroundColor = "#f0f6f0";
             prioritiesArr[i].style.color = "#222323";
             prioritiesArr[i].style.padding = "0";
         }
-    }
-}
-*/
+    })
+    
+}*/
 
+/*
+for (let i = 0; i < prioritiesArr.length; i++) {
+    if (selectedPriority == prioritiesArr[i]) {
+        selectedPriority.style.backgroundColor = "#222323";
+        selectedPriority.style.color = "#f0f6f0";
+        selectedPriority.style.padding = "1%, 0, 2%, 2%";
+    } else if (selectedPriority =! prioritiesArr[i]) {
+        selectedPriority.style.backgroundColor = "#f0f6f0";
+        selectedPriority.style.color = "#222323";
+        selectedPriority.style.padding = "0";
+    }
+};*/
+
+
+/*
+for (let i = 0; i <= prioritiesArr.length; i++) {
+    if (selectedPriority = prioritiesArr[i]) {
+        selectedPriority.addEventListener("click", () => {
+            selectedPriority.style.backgroundColor = "#222323";
+            selectedPriority.style.color = "#f0f6f0";
+            selectedPriority.style.padding = "1%, 0, 2%, 2%";
+            if (prioritiesArr[i] != selectedPriority) {
+                prioritiesArr[i].style.backgroundColor = "#f0f6f0";
+                prioritiesArr[i].style.color = "#222323";
+                prioritiesArr[i].style.padding = "0";
+            }
+        })
+    }
+        
+        
+    
+}
+
+
+*/
 
 //Task Create Button
 let itemCreateButton = document.createElement("h4");
